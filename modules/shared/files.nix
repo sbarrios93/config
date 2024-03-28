93b5ee1a6f34f13ec768986eb86bec2b8481cbd6
Sebastian Barrios Slight <sbarrios93@gmail.com>
2024-03-28 00:46:28 -0400

    # fix

--- modules/shared/files.nix
+++ modules/shared/files.nix
@@ -1,3 +1,14 @@
-{ pkgs, config, ... }:
+{ user, pkgs, config, ... }:
 
-{ }
+
+let
+  xdg_configHome = "${config.users.users.${user}.home}/.config";
+  xdg_dataHome = "${config.users.users.${user}.home}/.local/share";
+  xdg_stateHome = "${config.users.users.${user}.home}/.local/state";
+in
+{
+  home.file."${xdg_configHome}/nvim" = {
+    source = ../config/nvim;
+  };
+}
+
