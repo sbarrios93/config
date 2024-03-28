913c766fb8d7816be7852afe457e4ab8febdc807
Sebastian Barrios Slight <sbarrios93@gmail.com>
2024-03-28 01:39:17 -0400

    fixes

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -12,7 +12,11 @@ in
     autocd = true;
 
     initExtraFirst = ''
-        '';
+      if [[ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]]; then
+          . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+          . /nix/var/nix/profiles/default/etc/profile.d/nix.sh
+      fi
+    '';
   };
 
 
