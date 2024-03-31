5d86e2fc2bcc5a10b13da9fea3232ecb5a8fcc29
Sebastian Barrios Slight <sbarrios93@gmail.com>
2024-03-31 00:12:33 -0400

    disable homebrew

--- flake.nix
+++ flake.nix
@@ -8,24 +8,9 @@
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-homebrew = {
-      url = "github:zhaofengli-wip/nix-homebrew";
-    };
-    homebrew-bundle = {
-      url = "github:homebrew/homebrew-bundle";
-      flake = false;
-    };
-    homebrew-core = {
-      url = "github:homebrew/homebrew-core";
-      flake = false;
-    };
-    homebrew-cask = {
-      url = "github:homebrew/homebrew-cask";
-      flake = false;
-    };
   };
 
-  outputs = { self, darwin, nix-homebrew, homebrew-bundle, homebrew-core, homebrew-cask, home-manager, nixpkgs } @inputs:
+  outputs = { nixpkgs, ... } @inputs:
     let
       vars = import ./vars;
     in
