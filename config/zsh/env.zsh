8c9a5a944d1bf88553b3cad9d31eca5a25673c26
sbarrios93 <sbarrios93@gmail.com>
2024-09-19 10:14:02 -0700

    remove rye

--- config/zsh/env.zsh
+++ config/zsh/env.zsh
@@ -10,8 +10,6 @@ XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
 RUSTUP_HOME="${XDG_CONFIG_HOME:-$HOME/.config}/rustup"
 CARGO_HOME="${XDG_CONFIG_HOME:-$HOME/.config}/cargo"
 
-# RYE
-RYE_HOME="${XDG_CONFIG_HOME:-$HOME/.config}/rye"
 
 # 1Password
 OPENAI_API_KEY="op://Personal/OpenAI/token"
